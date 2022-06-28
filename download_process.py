8eb9f522314c4b488914ec3ce497dd31ad5ed90f
Luke Davis <lukelbd@gmail.com>
2022-06-27 20:04:02 -0600

    Update download script with feedback stuff

--- download_process.py
+++ download_process.py
@@ -2,16 +2,6 @@
 """
 File for downloading and merging relevant data.
 """
-# WARNING: List corrupt files that have to be manually deleted here. Can test using
-# e.g. for file in ta*nc; do echo $file && ncvardump ta $file >/dev/null; done
-# since often get HDF5 errors during processing but header can be read without error.
-# ./cmip6-picontrol-amon/clwvi_Amon_NorESM2-MM_piControl_r1i1p1f1_gn_132001-132912.nc
-# ./cmip6-picontrol-amon/clw_Amon_UKESM1-0-LL_piControl_r1i1p1f2_gn_196001-199912.nc
-# ./cmip6-picontrol-amon/cl_Amon_CNRM-CM6-1-HR_piControl_r1i1p1f2_gr_185001-185912.nc
-# ./cmip6-picontrol-amon/cl_Amon_CNRM-CM6-1-HR_piControl_r1i1p1f2_gr_186001-186912.nc
-# ./cmip6-picontrol-amon/cl_Amon_CNRM-CM6-1-HR_piControl_r1i1p1f2_gr_187001-187912.nc
-# Note only 3 files of CNRM-CM6-1-HR so far and last one is corrupt so should delete,
-# also there are no clw or cli files for this model, then add others as we go along.
 import cmip_data
 
 # Global arguments
@@ -46,15 +36,18 @@ import cmip_data
 # In sum we added 5 models but missed 9 models for a total of 4 models fewer (47 instead
 # of 52). We can rectify everything but the IPSL model, plus add the extra MIROC model,
 # to provide a total of 5 models more than Mark Zelinka (57 instead of 52).
-series = True  # get feedback time series?
-climate = True  # get all climate averages?
-feedbacks = True  # control and response data for feedbacks?
-constrain = True  # control data for constraints?
+series = True  # get feedback analysis time series?
+climate = True  # get various climate time averages?
+analysis = True  # control and response data for feedback analysis?
+constraints = True  # control data for emergent constraints?
+dependencies = True  # dependency data for emergent constraintss?
 circulation = True  # control and response data for implicit circulation stuff?
 explicit = True  # control and response data for explicit circulation stuff?
 download = False
-filter = True
+filter = False
 process = False
+feedbacks = True
+summarize = False
 
 # Pre-industrial control and response data for constraints, transport, and feedbacks
 # NOTE: The abrupt and control radiation data is needed for 'local feedback parameter'
@@ -74,12 +67,12 @@ process = False
 # precipitation, where assumption is 1) water/snow/ice transport is neglible compared to
 # humidity transport (valid since vertically integrated humidity is always larger
 # than vertically integrated water/snow/ice, should make plots to demonstrate), and
-# similarly 2) precipitation falls where it was formed (implying it equals the component
+# therefore 2) precipitation falls where it was formed (implying it equals the component
 # of vertically integrated latent heat released by forming hydrometeors not balanced
 # by latent heat absorbed by evaporating hydrometeors -- should research literature
-# on these assumptions). Armour et al. (2019) only mention falling snow but mistaken.
-# The LH flux convergence is then written dLH/dt = Lv * (pr - prsn) + Ls * prsn - hsls
-# where hsls is explicit or can be found with Lv * (evspsbl - sbl) + Ls * sbl (include
+# on these assumptions). Armour et al. (2019) only mention falling snow but incomplete.
+# The LH flux convergence is then written dLH/dt = Lv * (pr - prsn) + Ls * prsn - hfls
+# where hfls is explicit or can be found with Lv * (evspsbl - sbl) + Ls * sbl (include
 # the implicit calculation only for sanity checks since sbl is often not provided).
 # NOTE: The transport data permits sanity checks against energetic and water residuals.
 # Should produce comparison plots of dry transport derived from (1) energy balance
@@ -99,7 +92,7 @@ kw_constant = {
         'sftlf',  # land fraction (possibly useful for averages e.g. constraints)
     ],
 }
-kw_feedbacks = {
+kw_analysis = {
     'table': 'Amon',
     'experiment': ['piControl', 'abrupt-4xCO2'],
     'variable': [
@@ -119,9 +112,9 @@ kw_feedbacks = {
         'rsuscs',  # upwelling SW surface (clear-sky) (in response to downwelling)
         'rldscs',  # downwelling LW surface (clear-sky)
         'rsdscs',  # downwelling SW surface (clear-sky)
-        # 'rtmt',  # net TOA LW and SW (use individual components instead)
-        # 'rls',  # net surface LW (use individual components instead)
-        # 'rss',  # net surface SW (use individual components instead)
+        # 'rls',  # net surface LW (Emon table, use individual components instead)
+        # 'rss',  # net surface SW (Emon table, use individual components instead)
+        # 'rtmt',  # net TOA LW and SW (Amon table, use individual components instead)
     ],
 }
 kw_circulation = {
@@ -144,7 +137,7 @@ kw_circulation = {
         'zg',  # geopotential height for circulation
     ]
 }
-kw_constrain = {
+kw_constraints = {
     'table': 'Amon',
     'experiment': ['piControl', 'abrupt-4xCO2'],
     'variable': [
@@ -197,13 +190,13 @@ kw_explicit = {
 # NOTE: Facets and options supplied to constrain filter are standardized between
 # cmip5 and cmip6 synonyms. So can loop through both projects with about keywords.
 dicts = []
-if feedbacks:
-    dicts.append(kw_feedbacks)
+if analysis:
+    dicts.append(kw_analysis)
 if circulation:
     dicts.append(kw_circulation)
-if constrain:
-    dicts.append(kw_constrain)
-if constrain:
+if constraints:
+    dicts.append(kw_constraints)
+if dependencies:
     dicts.append(kw_dependencies)
 if explicit:
     dicts.append(kw_explicit)
@@ -223,11 +216,11 @@ if download:
                 folder = '~/scratch5'
             script = cmip_data.download_script(
                 folder,
-                project=project,
                 node='llnl',
                 openid='https://esgf-node.llnl.gov/esgf-idp/openid/lukelbd',
                 username='lukelbd',
                 flagship_filter=True,
+                project=project,
                 **kwargs,
             )
             unfiltered.append(script)
@@ -247,8 +240,10 @@ if filter:
             projects = ('CMIP6', 'CMIP5')
         if kwargs is kw_dependencies:
             always_exclude = {'table': 'AERmon', 'model': ['HadGEM3-GC31-LL', 'HadGEM3-GC31-MM']}  # noqa: E501
+        elif kwargs is kw_constraints:
+            always_exclude = {'experiment': 'abrupt4xCO2'}
         else:
-            always_exclude = {'variable': 'pfull'}
+            always_exclude = {}
         for project in projects:
             if kwargs is kw_dependencies:
                 folder = '~/scratch2/data-dependencies'
@@ -258,18 +253,18 @@ if filter:
                 folder = '~/scratch5'
             scripts = cmip_data.filter_script(
                 folder,
-                project=project,
                 maxyears=150,
                 endyears=False,
                 always_include=None,
                 always_exclude=always_exclude,
-                flagship_translate=True,
+                flagship_filter=True,
+                project=project,
                 **kwargs
             )
             filtered.extend(scripts)
 
 # Average and standardize the resulting files
-# NOTE: Running overwrite=True
+# TODO: Overwrite only piControl nodrift series that should end up failing
 # NOTE: Here follow Armour et al. 2019 methodology of taking difference between final
 # 30 years of the 150 years required by the DECK abrupt-4xco2 experiment protocol. Also
 # again exclude constraint data from processing (pfull is excluded in process_files).
@@ -285,7 +280,7 @@ if process:
                 projects = ('CMIP6', 'CMIP5')
             for project in projects:
                 experiments = {'piControl': 150, 'abrupt-4xCO2': (120, 150)}
-                if kwargs is kw_constrain:
+                if kwargs is kw_constraints:
                     del experiments['abrupt-4xCO2']
                 if not climate:
                     experiments.clear()
@@ -299,18 +294,18 @@ if process:
                         folder,
                         output='~/data',
                         search='~/scratch2/data-dependencies',
-                        project=project,
-                        flagship_translate=True,
                         method='gencon',
-                        nodrift=nodrift,
                         years=years,
                         climate=True,
-                        overwrite=nodrift,
+                        nodrift=nodrift,
+                        overwrite=False,
                         dryrun=False,
+                        flagship_filter=True,
+                        project=project,
                         **kw,
                     )
-                experiments = {'abrupt-4xCO2': 150, 'piCntrol': 150}
-                if kwargs is not kw_feedbacks:
+                experiments = {'abrupt-4xCO2': 150, 'piControl': 150}
+                if kwargs is not kw_analysis:
                     experiments.clear()
                 if not series:
                     experiments.clear()
@@ -320,20 +315,38 @@ if process:
                         folder,
                         output='~/scratch2/data-series',
                         search='~/scratch2/data-dependencies',
-                        project=project,
-                        flagship_translate=True,
                         method='gencon',
-                        nodrift=nodrift,
                         years=years,
                         climate=False,
-                        overwrite=nodrift,
+                        nodrift=nodrift,
+                        overwrite=False,
                         dryrun=False,
+                        flagship_filter=True,
+                        project=project,
                         **kw,
                     )
 
-# Calculate the correlation, regression, and ratio feedbacks
-if process and feedbacks:
-    pass
+# Calculate the response, control, and anomaly feedbacks (note control anomaly
+# feedbacks are impossible because cannot select a period to use for anomalies).
+# NOTE: The residual feedback will only be calculated if all kernels
+# for the associated flux are requested. Otherwise this is bypassed.
+if feedbacks:
+    for nodrift in (True, False):
+        # methods = ('response', 'control', 'anomalies')
+        methods = ('response', 'control')
+        for method in methods:
+            projects = ('CMIP6', 'CMIP5')
+            for project in projects:
+                cmip_data.process_feedbacks(
+                    '~/data',
+                    '~/scratch2/data-series',
+                    method=method,
+                    project=project,
+                    flagship_filter=True,
+                    nodrift=nodrift,
+                    overwrite=False,
+                    testing=True,
+                )
 
 # Update the summary logs once finished
 # NOTE: The missing files for different feedback variables are tabulated below.
@@ -360,7 +373,7 @@ if process and feedbacks:
 # rldscs: CAMS-CSM1-0, GFDL-ESM4, MCM-UA-1-0, NorESM2-LM, NorESM2-MM, TaiESM1
 # rsds: MCM-UA-1-0
 # rsdscs: CAMS-CSM1-0, GFDL-ESM4, MCM-UA-1-0
-if False:
+if summarize:
     for project in ('cmip6', 'cmip5'):
         folders_downloads = ('~/scratch2', '~/scratch5')
         folders_processed = ('~/scratch2/data-series', '~/data')
@@ -370,6 +383,11 @@ if False:
             flagship_translate=True,
         )
         cmip_data.summarize_processed(
+            *folders_processed,
+            project=project,
+            flagship_translate=True,
+        )
+        cmip_data.summarize_ranges(
             *folders_processed,
             project=project,
             flagship_translate=True,
